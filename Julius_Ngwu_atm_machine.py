0e3314dcc4e4f788043d0fbe4ff311725d965f4e
Shaheensha-2244 <gs.shaheensha20052003@gmail.com>
2024-12-12 22:46:53 +0530

    Update Julius_Ngwu_atm_machine.py

--- Julius_Ngwu_atm_machine.py
+++ Julius_Ngwu_atm_machine.py
@@ -36,10 +36,10 @@ while status:
 # User wants to withdraw cash
         if Option==2:
             print("Balance  ₦",balance)
-            Withdraw=float(input("Enter Withdraw amount ₦ "))
-            if (Withdraw < balance):
-                if Withdraw>0:
-                    forewardbalance=(balance-Withdraw)
+            Deposit=float(input("Enter Deposit amount ₦ "))
+            if (Deposit < balance):
+                if Deposit>0:
+                    remainingbalance=(balance-Deposit)
                     print("Remaining Balance  ₦",remainingbalance)
                     follow_up = print("Do You want to perform another transaction Yes(y)/ No(n)")
                     trigger_status = input("Enter response ")
@@ -48,10 +48,10 @@ while status:
                     if trigger_status == 'N':
                         status = False
                     
-                elif Withdraw>balance:
+                elif Deposit>balance:
                     print("No funs in account")
                 else:
-                    print("None withdraw made")
+                    print("None  made")
             else:
                 print("Insufficient balance")
                 follow_up = print("Do You want to perform another transaction Yes(y)/ No(n)")
@@ -64,9 +64,9 @@ while status:
 # User wants to deposit cash
         if Option==3:
             print("Balance  ₦",balance)
-            Deposit=float(input("Enter deposit amount ₦ "))
-            if Deposit>0:
-                remainingbalance=(balance+Deposit)
+            Withdraw=float(input("Enter withdraw amount ₦ "))
+            if Withdraw>0:
+                remainingbalance=(balance+Withdraw)
                 print("Remainingbalance  ₦ ",remainingbalance)
                 follow_up = print("Do You want to perform another transaction Yes(y)/ No(n)")
                 trigger_status = input("Enter response ")
@@ -75,7 +75,7 @@ while status:
                 if trigger_status == 'N':
                     status = False
             else:
-                print("None deposit made")
+                print("None withdarw made")
 
 # User wants to exit 
         if Option==4:
